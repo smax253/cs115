0977a157f905b98b0ffef4fc15811ae64242368c
smax253 <smax253@gmail.com>
2018-09-21 12:47:29 -0400

    Added existing files prior to 9/21/18

---
+++ demo1.py
@@ -0,0 +1,35 @@
+from cs115 import *
+
+def add(x,y):
+    return x+y
+
+def f(L):
+    '''assume L is a list of numbers'''
+    return reduce(add,L)
+
+def mult(x,y): return x*y
+
+def fact(N):
+    return reduce(mult,range(1,N+1))
+
+def span(L):
+    '''return difference between max and min of list L'''
+    return reduce(max,L)-reduce(min,L);
+
+def square(N):
+    '''returns square of N'''
+    return N*N;
+
+def gauss(N):
+    '''returns sum of numbers from 1 to N'''
+    return reduce(add,range(1,N+1))
+
+def sumOfSquares(N):
+    '''returns sum of squares from 1 to N'''
+    return reduce(add,map(square,range(1,N+1)))
+
+def reverse2(L):
+    def rev(acc,L):
+        if L==[]: return acc
+        else: return rev([L[0]]+acc, L[1:])
+    return rev([],L)
