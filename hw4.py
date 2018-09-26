dbfc96144bfad5247b68476dce16b9d492508660
smax253 <smax253@gmail.com>
2018-09-26 11:54:22 -0400

    Completed lab4 and hw4

---
+++ hw4.py
@@ -0,0 +1,16 @@
+"""
+    Max Shi
+    9/26/18
+    I pledge my honor that I have abided by the Stevens Honor System
+"""
+
+def adjSum(L):
+    L=L+[0]
+    if L[1] == 0: return [1]
+    else: return [L[0]+L[1]]+adjSum(L[1:])
+def pascal_row(N):
+    if N == 0: return [1]
+    else: return [1]+adjSum(pascal_row(N-1))
+def pascal_triangle(N):
+    if(N==0): return [[1]]
+    else: return pascal_triangle(N-1)+[[1]+adjSum(pascal_row(N-1))]
---
+++ lab4.py
@@ -0,0 +1,14 @@
+"""
+    Max Shi
+    9/26/18
+    I pledge my honor that I have abided by the Stevens Honor System
+"""
+
+def knapsack(cap, items):
+    if items == []: return [0,[]]
+    elif items[0][0]>cap: return knapsack(cap, items[1:])
+    else:
+        use = knapsack(cap-items[0][0],items[1:])
+        useList = [items[0][1]+use[0],[items[0]]+use[1]]
+        lose = knapsack(cap,items[1:])
+        return useList if useList[0]>lose[0] else lose
