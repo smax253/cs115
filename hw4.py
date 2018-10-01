ad6a108ed1f0b77419afab868090c7cf85068c1b
smax253 <smax253@gmail.com>
2018-10-01 09:58:26 -0400

    Fixed adjSum to be more practical, fixed pascal_triangle to make sense

--- hw4.py
+++ hw4.py
@@ -6,19 +6,18 @@
 
 def adjSum(L):
 
-    '''Returns a list of the sums of the given elements in the list with a 1 on the end'''
+    '''Returns a list of the sums of the given elements in the list'''
     if L[0] == 1: L=L+[0]
-
-    if L[1] == 0: return [1]
+    if L[1] == 0: return []
     else: return [L[0]+L[1]]+adjSum(L[1:])
 def pascal_row(N):
     '''Returns the given row N of Pascal's Triangle'''
     if N == 0: return [1]
-    else: return [1]+adjSum(pascal_row(N-1))
+    else: return [1]+adjSum(pascal_row(N-1))+[1]
 def pascal_triangle(N):
     '''Returns a list of Pascal's triangle's rows from row 0 up to row N'''
     if(N==0): return [[1]]
-    else: return pascal_triangle(N-1)+[[1]+adjSum(pascal_row(N-1))]
+    else: return pascal_triangle(N-1)+[pascal_row(N)]
 def test_pascal_row():
     '''Runs four test cases on pascal_row(N)'''
     assert(pascal_row(3) == [1,3,3,1])
