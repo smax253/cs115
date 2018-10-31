c97190fe3ad8721ab6a1a99cfd09255f7916e459
smax253 <smax253@gmail.com>
2018-10-31 10:11:16 -0400

    inclass exercise

--- demo1.py
+++ demo1.py
@@ -34,6 +34,31 @@ def reverse2(L):
         else: return rev([L[0]]+acc, L[1:])
     return rev([],L)
 
+def factorial(N):
+    ans = 1
+    for x in range(1,N+1):
+        ans*=x
+    return ans
 
+def mapSqr(L):
+    ans = []
+    for x in range(len(L)):
+        ans += [L[x]**2]
+    return ans
+
+
+
+def fib(N):
+    first = 0
+    second = 1
+    if N==0: return first
+    elif N==1: return second
+    while N>1:
+        third = second+first
+        first = second
+        second = third
+        N-=1
+    return second
+        
 
 
---
+++ ex.py
@@ -0,0 +1,11 @@
+
+def prime(N):
+    if N == '2': return True
+    else:
+        return !N
+def primeFac(N):
+    L = []
+    for x in range(2,N):
+        if(N%x==0):
+            if()==0:
+               L+=[x]
